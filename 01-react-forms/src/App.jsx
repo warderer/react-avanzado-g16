d5b2fd11d23281e6b4a980091041031a509f50ab
César Guerra <warderer@gmail.com>
2022-09-13 21:23:36 -0500

    ✨ Formulario con custom hook useForm

--- 01-react-forms/src/App.jsx
+++ 01-react-forms/src/App.jsx
@@ -1,11 +1,12 @@
 import './App.css'
+import FormHook from './components/FormHook'
 import RefDom from './components/RefDom'
 import SimpleForm from './components/SimpleForm'
 
 function App () {
   return (
     <div className='App'>
-      <SimpleForm />
+      <FormHook />
     </div>
   )
 }
--- 01-react-forms/src/components/FormHook.jsx
+++ 01-react-forms/src/components/FormHook.jsx
@@ -1,4 +1,6 @@
 import { useState, useEffect } from 'react'
+import useForm from '../hooks/useForm'
+import logo from '../assets/react.svg'
 
 const FormHook = () => {
   /* Paso 1: Crear un ÚNICO estado con toda la info en forma de objeto */
@@ -10,6 +12,13 @@ const FormHook = () => {
     email: '',
     password: ''
   })
+
+  const sendData = (data) => {
+    console.log(data)
+  }
+
+  /* Paso 4: Uso mi custom Hook de useForm */
+  const { input, handleInputChange, handleSubmit } = useForm(sendData, datos)
 
   /* Paso 2: Voy a simular traer datos de una API y prerellenar el formulario */
   useEffect(() => {
@@ -27,8 +36,81 @@ const FormHook = () => {
     }, 2000)
   }, [])
 
+  /* Paso 3: Crear mi formulario */
   return (
-    <div>FormHook</div>
+    <div className='login'>
+      <div className='login-container'>
+        <img src={logo} alt='logo' className='logo' width='200px' height='200px' />
+        <form>
+          <label htmlFor='nombre'>Nombre</label>
+          <input
+            type='text'
+            name='nombre'
+            placeholder='Tu nombre'
+            id='nombre'
+            onChange={handleInputChange}
+            value={input.nombre}
+          />
+
+          <label htmlFor='apellido'>Apellido</label>
+          <input
+            type='text'
+            name='apellido'
+            placeholder='Tu apellido'
+            id='apellido'
+            onChange={handleInputChange}
+            value={input.apellido}
+          />
+
+          <label htmlFor='edad'>Edad</label>
+          <input
+            type='number'
+            name='edad'
+            placeholder='Tu edad'
+            id='edad'
+            onChange={handleInputChange}
+            value={input.edad}
+          />
+
+          <label htmlFor='genero'>Genero</label>
+          <select
+            name='genero'
+            id='genero'
+            onChange={handleInputChange}
+            value={input.genero}
+          >
+            <option value=''>Elige un genero</option>
+            <option value='M'>Masculino</option>
+            <option value='F'>Femenino</option>
+            <option value='O'>Otro</option>
+          </select>
+
+          <label htmlFor='email'>Email</label>
+          <input
+            type='text'
+            name='email'
+            placeholder='correo@mail.com'
+            id='email'
+            onChange={handleInputChange}
+            value={input.email}
+          />
+
+          <label htmlFor='password'>Password</label>
+          <input
+            type='password'
+            name='password'
+            placeholder='******'
+            id='password'
+            onChange={handleInputChange}
+            value={input.password}
+          />
+
+          <button onClick={handleSubmit}>Iniciar Sesión</button>
+
+        </form>
+
+      </div>
+    </div>
   )
 }
 export default FormHook
