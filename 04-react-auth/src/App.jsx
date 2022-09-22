7eb2b04d8bd41e302488df110d3c1d5f2477cc2f
César Guerra <warderer@gmail.com>
2022-09-21 20:44:41 -0500

    🎉 React Auth - Project Init

---
+++ 04-react-auth/src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App () {
+  return (
+    <>
+      <h1>React Auth</h1>
+    </>
+  )
+}
+
+export default App
---
+++ 04-react-auth/src/pages/index.js
@@ -0,0 +1,11 @@
+import Home from './Home'
+import Login from './Login'
+import Secret from './Secret'
+import Signup from './Signup'
+
+export {
+  Home,
+  Login,
+  Secret,
+  Signup
+}
