825765f3e049a201e81323788c151f48e7bc94bf
César Guerra <warderer@gmail.com>
2022-09-21 21:37:09 -0500

    ✨ Header con Sass y BEM

--- 04-react-auth/src/App.jsx
+++ 04-react-auth/src/App.jsx
@@ -1,8 +1,10 @@
-import './App.css'
+import '@/App.css'
+import { Header } from '@/components/Header'
 
 function App () {
   return (
     <>
+      <Header />
       <h1>React Auth</h1>
     </>
   )
---
+++ 04-react-auth/src/components/Header/Header.jsx
@@ -0,0 +1,24 @@
+import './Header.scss'
+
+const Header = () => {
+  return (
+    <nav className='header'>
+      <a href='/' className='header__logo'>LOGO</a>
+      <ul className='header__nav-list'>
+        <li className='header__list-item'>
+          <a href='/' className='header__item-link header__item-link--is-active'>Home</a>
+        </li>
+        <li className='header__list-item'>
+          <a href='/login' className='header__item-link'>Login</a>
+        </li>
+        <li className='header__list-item'>
+          <a href='/signup' className='header__item-link'>Signup</a>
+        </li>
+        <li className='header__list-item'>
+          <a href='/secret' className='header__item-link'>Secret</a>
+        </li>
+      </ul>
+    </nav>
+  )
+}
+export default Header
