b1f9b78803640c27833af5a55d2c6adcd10b533a
César Guerra <warderer@gmail.com>
2022-09-26 21:08:13 -0500

    💄 UI de Login con Boostrap

--- 04-react-auth/src/pages/Login.jsx
+++ 04-react-auth/src/pages/Login.jsx
@@ -1,6 +1,42 @@
+import logo from '@/assets/react.svg'
+import '@/assets/css/form.css'
+
 const Login = () => {
   return (
-    <div>Login</div>
+    <main className='form-signin w-100 m-auto'>
+      <form>
+        <img className='mb-4' src={logo} alt='' width='72' height='57' />
+        <h1 className='h3 mb-3 fw-normal'>Please sign in</h1>
+
+        <div className='form-floating'>
+          <input
+            type='email'
+            className='form-control'
+            id='floatingInput'
+            name='email'
+            value=''
+            onChange={() => {}}
+            placeholder='name@example.com'
+          />
+          <label htmlFor='floatingInput'>Email address</label>
+        </div>
+        <div className='form-floating'>
+          <input
+            type='password'
+            className='form-control'
+            id='floatingPassword'
+            name='password'
+            value=''
+            onChange={() => {}}
+            placeholder='Password'
+          />
+          <label htmlFor='floatingPassword'>Password</label>
+        </div>
+
+        <button className='w-100 btn btn-lg btn-primary' type='submit'>Sign in</button>
+        <p className='mt-5 mb-3 text-muted'>© 2017–2022</p>
+      </form>
+    </main>
   )
 }
 export default Login
