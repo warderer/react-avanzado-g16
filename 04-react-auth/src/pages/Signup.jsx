c8bb5b8af8869e688b782d2555232658b09237bf
César Guerra <warderer@gmail.com>
2022-09-28 20:57:01 -0500

    ♻️ Cambio de nombres para evitar conflicto en userServices

--- 04-react-auth/src/pages/Signup.jsx
+++ 04-react-auth/src/pages/Signup.jsx
@@ -1,6 +1,6 @@
 import useForm from '@/hooks/useForm'
 import { useNavigate } from 'react-router-dom'
-import { registerUser } from '@/services/userServices'
+import { registerUserService } from '@/services/userServices'
 import logo from '@/assets/react.svg'
 import '@/assets/css/form.css'
 
@@ -8,7 +8,7 @@ const Signup = () => {
   const navigate = useNavigate()
   const sendData = async (data) => {
     try {
-      const result = await registerUser(data)
+      const result = await registerUserService(data)
       if (result.status === 200) {
         navigate('/login')
       }
--- 04-react-auth/src/services/userServices.js
+++ 04-react-auth/src/services/userServices.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 const BASE_URL = 'https://ecomerce-master.herokuapp.com/api/v1'
 
-const registerUser = (data) => axios.post(`${BASE_URL}/signup`, data)
-const loginUser = (data) => axios.post(`${BASE_URL}/login`, data)
+const registerUserService = (data) => axios.post(`${BASE_URL}/signup`, data)
+const loginUserService = (data) => axios.post(`${BASE_URL}/login`, data)
 
-export { registerUser, loginUser }
+export { registerUserService, loginUserService }
