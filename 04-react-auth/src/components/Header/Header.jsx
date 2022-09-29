d16ebfc190cf57973d7c477cdb5e844952c2a0b3
César Guerra <warderer@gmail.com>
2022-09-28 21:32:34 -0500

    ✨ Header con Context

--- 04-react-auth/src/components/Header/Header.jsx
+++ 04-react-auth/src/components/Header/Header.jsx
@@ -1,23 +1,49 @@
+import { useContext } from 'react'
+import { AuthContext } from '@/context/AuthContext'
 import { Link } from 'react-router-dom'
 import './Header.scss'
 
 const Header = () => {
+  const { isAuth, logout } = useContext(AuthContext)
   return (
     <nav className='header'>
       <Link to='/' className='header__logo'>LOGO</Link>
       <ul className='header__nav-list'>
+
         <li className='header__list-item'>
           <Link to='/' className='header__item-link header__item-link--is-active'>Home</Link>
         </li>
-        <li className='header__list-item'>
-          <Link to='/login' className='header__item-link'>Login</Link>
-        </li>
-        <li className='header__list-item'>
-          <Link to='/signup' className='header__item-link'>Signup</Link>
-        </li>
-        <li className='header__list-item'>
-          <Link to='/secret' className='header__item-link'>Secret</Link>
-        </li>
+
+        {
+        !isAuth
+          ? (
+            <>
+              <li className='header__list-item'>
+                <Link to='/login' className='header__item-link'>Login</Link>
+              </li>
+
+              <li className='header__list-item'>
+                <Link to='/signup' className='header__item-link'>Signup</Link>
+              </li>
+            </>
+            )
+          : (
+            <>
+              <li className='header__list-item'>
+                <Link to='/secret' className='header__item-link'>Secret</Link>
+              </li>
+              <li className='header__list-item'>
+                <Link
+                  to='/'
+                  className='header__item-link'
+                  onClick={logout}
+                >Logout
+                </Link>
+              </li>
+            </>
+            )
+        }
+
       </ul>
     </nav>
   )
