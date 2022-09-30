60a3198b9a119e9155d58f1bf14959a0d8c35952
César Guerra <warderer@gmail.com>
2022-09-29 19:32:50 -0500

    ✨ Consumo Endpoint Protegido de User en Dashboard

--- 04-react-auth/src/services/userServices.js
+++ 04-react-auth/src/services/userServices.js
@@ -2,7 +2,17 @@ import axios from 'axios'
 
 const BASE_URL = 'https://ecomerce-master.herokuapp.com/api/v1'
 
+const token = window.localStorage.getItem('token')
+const config = {
+  headers: { Authorization: `JWT ${token}` }
+}
+
 const registerUserService = (data) => axios.post(`${BASE_URL}/signup`, data)
 const loginUserService = (data) => axios.post(`${BASE_URL}/login`, data)
+const getSingleUser = (id) => axios.get(`${BASE_URL}/user/${id}`, config)
 
-export { registerUserService, loginUserService }
+export {
+  registerUserService,
+  loginUserService,
+  getSingleUser
+}
